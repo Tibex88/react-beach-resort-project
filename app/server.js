b30a120e527a499909b289961d828c95bc9f08eb
Tibebe Solomon <87706650+Tibex88@users.noreply.github.com>
2024-01-10 10:30:30 +0300

    added winston logging

--- app/server.js
+++ app/server.js
@@ -1,14 +1,21 @@
 const mongoose = require("mongoose");
 const app = require("./index");
-
+const logger = require("./utils/logger.js")
 require("./config/db_Connection")
 require("dotenv").config({
   path: "./config.env"
 });
 
+logger.error("error") ;
+logger.warn("warn") ;
+logger.info("info") ;
+logger.verbose("verbose") ;
+logger.debug("debug") ;
+logger.silly("silly") ;
+
 const port = process.env.PORT;
 app.listen(port, () => {
-  console.log(`The port is listening...at ${port}`);
+  logger.info(`The port is listening...at ${port}`);
 });
 
 process.on("unhandledRejection", (err) => {
